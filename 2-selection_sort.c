95e1e4698571d125d92005b8b8b1159b8586938f
DarkWeirdo <sonic_unleashed@live.com>
2024-01-23 00:05:24 +0200

    selection_sort + sort header fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,37 +7,36 @@
  */
 void swap_integers(int *a, int *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 /**
- * selection_sort - Sorts an array of integers in ascending order using the
+ * selection_sort - Sorts an array of integers in using the
  * Selection sort algorithm.
  * @array: The array to be sorted
  * @size: Number of elements in @array
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+    size_t i, j, min_idx;
 
-	if (size < 2)
-		return;
+    if (size < 2)
+        return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
-		}
-		if (min_idx != i)
-		{
-			swap_integers(&array[min_idx], &array[i]);
-			print_array(array, size);
-		}
-	}
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+        {
+            swap_integers(&array[min_idx], &array[i]);
+            print_array(array, size);
+        }
+    }
 }
-
